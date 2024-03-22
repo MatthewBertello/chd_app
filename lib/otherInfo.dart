9a8483c2b67dcdb115dc4b8b2b6b1fb692dcb536
Matthew Steffens <imatts004@gmail.com>
2024-03-22 14:47:18 -0500

    Made a screen for "Other Info"

---
+++ lib/otherInfo.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class OtherInfo extends StatelessWidget {
+  const OtherInfo({super.key});
+
+  final String bullet = "\u2022 ";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Healthcare Resources', style: TextStyle(fontWeight: FontWeight.bold)),
+            centerTitle: true,
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white),
+        body: Padding(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0, top: 8.0),
+            child: Column(
+              children: [
+                // the box containing the information (can be copy and pasted)
+                Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), border: Border.all()),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Align(
+                            alignment: Alignment.center,
+                            child: Text("Category 1", style: TextStyle(fontWeight: FontWeight.bold))),
+                          Text("$bullet Dummy link 1"),
+                          Text("$bullet Dummy link 2"),
+                          Text("$bullet Dummy link 3"),
+                        ],
+                      ),
+                    )
+                  ),
+                  const SizedBox(height: 20.0),
+
+                  // the 2nd box displayed
+                  Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), border: Border.all()),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Align(
+                            alignment: Alignment.center, 
+                            child: Text("Category 2", style: TextStyle(fontWeight: FontWeight.bold))),
+                          Text("$bullet Dummy link 1"),
+                          Text("$bullet Dummy link 2"),
+                          Text("$bullet Dummy link 3"),
+                        ],
+                      ),
+                    )
+                  ),
+                  const SizedBox(height: 20.0),
+
+                  // the 3rd box displayed
+                  Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), border: Border.all()),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Align(
+                              alignment: Alignment.center, 
+                              child: Text("Category 3", style: TextStyle(fontWeight: FontWeight.bold))),
+                          Text("$bullet Dummy link 1"),
+                          Text("$bullet Dummy link 2"),
+                          Text("$bullet Dummy link 3"),
+                        ],
+                      ),
+                    )
+                  ),
+              ],
+            )
+        )
+    );
+  }
+}
