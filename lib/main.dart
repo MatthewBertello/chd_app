47293d9bf70cce512b5f554ec92b2023f6be35c7
leepachia <pachia0lee@gmail.com>
2024-03-28 17:27:00 -0500

    Got rid of the theme for MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -101,7 +101,6 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: false),
       home: DefaultTabController(
         length: tabViews.length,
         // If the user is logged in, display overview with tabbed navigation, otherwise display the launchscreen
