d84cd63ba5d31e0e71b7c2843fdccea2ed6e3a14
leepachia <pachia0lee@gmail.com>
2024-03-28 17:26:32 -0500

    Added a title and search bar in the app bar

--- lib/shareScreen.dart
+++ lib/shareScreen.dart
@@ -1,13 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'test_model.dart';
 
 class ShareScreen extends StatelessWidget {
   const ShareScreen({required this.model});
   final TestModel model;
 
-  @override 
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Jane Doe')),);
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: Colors.red,
+      title: const Text(
+        'Share With Others',
+        style: TextStyle(color: Colors.white),
+      ),
+      bottom: const PreferredSize(
+        preferredSize: Size.fromHeight(40.0),
+            child: TextField(
+              maxLines: 1,
+              style: TextStyle(fontSize: 15),
+              decoration: InputDecoration(
+                  suffixIcon: IconButton(onPressed: null, icon: Icon(Icons.search)),
+                  hintText: 'Find a Member',
+                  filled: true,
+                  fillColor: Colors.white),
+            ),
+          ),
+        ),
+      );
   }
-
 }
