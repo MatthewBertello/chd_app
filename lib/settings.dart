ebdd9d34b200d61d3e18f19cc9c2110c6bca790d
leepachia <pachia0lee@gmail.com>
2024-04-01 20:37:54 -0500

    Started settings screen

---
+++ lib/settings.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'test_model.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({super.key, required this.model});
+  final TestModel model;
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
