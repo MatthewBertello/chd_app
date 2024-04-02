e7d64ec73fbfa3ff72b635672ae73cddb27063ff
leepachia <pachia0lee@gmail.com>
2024-04-01 20:46:50 -0500

    Made an app bar for settings.

--- lib/settings.dart
+++ lib/settings.dart
@@ -6,8 +6,27 @@ class Settings extends StatelessWidget {
   const Settings({super.key, required this.model});
   final TestModel model;
 
+  // Builds the app bar
+  // TODO: Make text white
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+        title: const Text("Settings"),
+        flexibleSpace: Container(
+            decoration: BoxDecoration(
+                // Gives the app bar a gradient red color
+                gradient: LinearGradient(colors: <Color>[
+          const Color.fromARGB(255, 249, 0, 0).withOpacity(0.9),
+          const Color.fromARGB(223, 189, 0, 0).withOpacity(0.9)
+        ]))),
+        toolbarHeight: 100,
+        );
+  }
+
   @override 
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: buildAppBar(context),
+      // TODO: body of scaffold
+    );
   }
 }
