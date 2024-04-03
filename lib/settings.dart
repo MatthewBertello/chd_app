dddc3cda19390626108b345ccb3051531ea9f85a
leepachia <pachia0lee@gmail.com>
2024-04-03 12:55:58 -0500

    Added const modifiers.

--- lib/settings.dart
+++ lib/settings.dart
@@ -7,31 +7,31 @@ class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Settings'), centerTitle: true),
+        appBar: AppBar(title: const Text('Settings'), centerTitle: true),
         body: SingleChildScrollView( // Wrapped with SingleChildScrollView to avoid overflow when keyboard appears
           padding: const EdgeInsets.all(75),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start, // Adjusted to start to avoid centering vertically
             crossAxisAlignment: CrossAxisAlignment.start, // Stretch to width of the screen
             children: <Widget>[
-              TextField(
+              const TextField(
                 decoration: InputDecoration(
                  hintText: 'Current Email',
                   border: OutlineInputBorder(),
                   contentPadding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0), // Add padding
                 ),
               ),
-              SizedBox(height: 16.0),
-              TextField(
+              const SizedBox(height: 16.0),
+              const TextField(
                 decoration: InputDecoration(
                   hintText: 'New Email',
                   border: OutlineInputBorder(),
                   contentPadding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0), // Add padding
                 ),
               ),
-              SizedBox(height: 60.0),
-              SizedBox(height: 16.0),
-              TextField(
+              const SizedBox(height: 60.0),
+              const SizedBox(height: 16.0),
+              const TextField(
                 decoration: InputDecoration(
                   hintText: 'Current Password',
                   border: OutlineInputBorder(),
@@ -39,8 +39,8 @@ class Settings extends StatelessWidget {
                 ),
                 obscureText: true, // Hide password
               ),
-              SizedBox(height: 16.0),
-              TextField(
+              const SizedBox(height: 16.0),
+              const TextField(
                 decoration: InputDecoration(
                   hintText: 'New Password',
                   border: OutlineInputBorder(),
@@ -48,8 +48,8 @@ class Settings extends StatelessWidget {
                 ),
                 obscureText: true, // Hide password
               ),
-              SizedBox(height: 16.0),
-              TextField(
+              const SizedBox(height: 16.0),
+              const TextField(
                 decoration: InputDecoration(
                   hintText: 'Confirm Password',
                   border: OutlineInputBorder(),
@@ -57,25 +57,25 @@ class Settings extends StatelessWidget {
                 ),
                 obscureText: true, // Hide password
               ),
-              SizedBox(height: 24.0),
+              const SizedBox(height: 24.0),
                  TextButton(
                 onPressed: () {},
                 style: TextButton.styleFrom(
                   foregroundColor: const Color.fromARGB(255, 3, 46, 81),
                 ),
-                child: Text('Delete Account'),
+                child: const Text('Delete Account'),
               ),
               ElevatedButton(
                 onPressed: () {},
                 style: ButtonStyle(
                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
+                    const RoundedRectangleBorder(
                       borderRadius: BorderRadius.zero, // Makes the button rectangular
                       side: BorderSide( width: 2.0), // border for button
                     ),
                   ),
                 ), 
-                child: Padding(
+                child: const Padding(
                   padding: EdgeInsets.symmetric(vertical: 15.0), // Add padding
                   child: Text('Submit'),
                 ),
