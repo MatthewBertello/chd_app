80b3593fddc26271315c3681af5333aa632f7e92
leepachia <pachia0lee@gmail.com>
2024-04-03 17:29:19 -0500

    Added some comments and added a textbutton for forgot password.

--- lib/login.dart
+++ lib/login.dart
@@ -10,11 +10,10 @@ class Login extends StatelessWidget {
     return Scaffold(
       body: Stack(
           children: [
-            Container(
+            Container( // Gives app a gradient background color
               decoration: BoxDecoration(
-              // Gives the app bar a gradient red color
               gradient: LinearGradient(colors: <Color>[
-        Color.fromARGB(255, 0, 4, 83).withOpacity(0.9),
+        const Color.fromARGB(255, 0, 4, 83).withOpacity(0.9),
         const Color.fromARGB(223, 189, 0, 0).withOpacity(0.9)
       ]))
             ),
@@ -24,11 +23,11 @@ class Login extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Padding(
+                Padding( // Congenital heart disease ribbon
                   padding: const EdgeInsets.only(bottom: 20),
                   child: Image.network('https://www.conqueringchd.org/wp-content/uploads/2020/07/awareness-ribbon-300x300.png',width:100.0, fit:BoxFit.cover)),
 
-                const Padding(
+                const Padding( // Textfield to enter email
                   padding: EdgeInsets.only(bottom: 20),
                   child: TextField(
                         decoration: InputDecoration(
@@ -42,7 +41,7 @@ class Login extends StatelessWidget {
                       ),
                 ),
                         
-                const Padding(
+                const Padding( // Textfield to enter password
                   padding: EdgeInsets.only(bottom: 20),
                   child: TextField(
                         decoration: InputDecoration(
@@ -63,13 +62,16 @@ class Login extends StatelessWidget {
                     updatePage(2);
                   },
                 ),
+
                 ElevatedButton(
                   style: const ButtonStyle(fixedSize: MaterialStatePropertyAll<Size>(Size(300, 20))),
                   child: Text('Sign in', style: TextStyle(color: Colors.indigo[900])),
                   onPressed: () {
                     updatePage(1);
                   },
-                )
+                ),
+
+                const TextButton(onPressed: null, child: Text('Forgot Password', style: TextStyle(color: Colors.white))),
               ],
                         ),
             ),]
