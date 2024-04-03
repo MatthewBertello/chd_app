9243042a0c286e8b077a7ee531a9503dfce0fa8e
leepachia <pachia0lee@gmail.com>
2024-04-03 16:57:19 -0500

    Deleted app bar and added textfields to login page.

--- lib/login.dart
+++ lib/login.dart
@@ -2,43 +2,79 @@ import 'package:chd_app/signUp.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-  Login({super.key, required this.updatePage});
+  const Login({super.key, required this.updatePage});
 
   final void Function(int) updatePage;
-
-  // Builds the app bar for login page
-  AppBar buildAppBar(BuildContext context) {
-    return AppBar(
-      flexibleSpace: Container(
-          decoration: BoxDecoration(
-              // Gives the app bar a gradient red color
-              gradient: LinearGradient(colors: <Color>[
-        const Color.fromARGB(255, 249, 0, 0).withOpacity(0.9),
-        const Color.fromARGB(223, 189, 0, 0).withOpacity(0.9)
-      ]))),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(context),
-      body: Column(
-        children: [
-          ElevatedButton(
-            child: Text('Sign up'),
-            onPressed: () {
-              updatePage(2);
-            },
-          ),
-          ElevatedButton(
-            child: Text('Sign in'),
-            onPressed: () {
-              updatePage(1);
-            },
-          )
-        ],
-      ),
-    );
+      body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+              // Gives the app bar a gradient red color
+              gradient: LinearGradient(colors: <Color>[
+        Color.fromARGB(255, 0, 4, 83).withOpacity(0.9),
+        const Color.fromARGB(223, 189, 0, 0).withOpacity(0.9)
+      ]))
+            ),
+
+            Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Image.network('https://www.conqueringchd.org/wp-content/uploads/2020/07/awareness-ribbon-300x300.png',width:100.0, fit:BoxFit.cover)),
+
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: TextField(
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: 'Email',
+                            enabledBorder: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.all(8.0),
+                            constraints: BoxConstraints(maxHeight: 45.0, maxWidth: 300)),
+                      ),
+                ),
+                        
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: TextField(
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: 'Password',
+                            enabledBorder: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.all(8.0),
+                            constraints: BoxConstraints(maxHeight: 45.0, maxWidth: 300)),
+                      ),
+                ),
+                        
+                ElevatedButton(
+                  style: const ButtonStyle(fixedSize: MaterialStatePropertyAll<Size>(Size(300, 20))),
+                  child: Text('Sign up', style: TextStyle(color: Colors.indigo[900]),),
+                  onPressed: () {
+                    updatePage(2);
+                  },
+                ),
+                ElevatedButton(
+                  style: const ButtonStyle(fixedSize: MaterialStatePropertyAll<Size>(Size(300, 20))),
+                  child: Text('Sign in', style: TextStyle(color: Colors.indigo[900])),
+                  onPressed: () {
+                    updatePage(1);
+                  },
+                )
+              ],
+                        ),
+            ),]
+        ),
+      );
   }
 }
