cf95d44eb193d827c291becc0190c6c29110bbb0
leepachia <pachia0lee@gmail.com>
2024-04-10 14:01:02 -0500

    Added some comments

--- lib/screens/burger_menu.dart
+++ lib/screens/burger_menu.dart
@@ -9,15 +9,16 @@ class BurgerMenu extends StatelessWidget {
   // A list of other pages for the app
   List<dynamic> otherPages = [["Settings",const Settings()], ["Find a member",ShareScreen(model: TestModel())], ["Resources",const OtherInfo()]];
 
+  // Builds a list of all the other pages that are clickable
   Widget buildBody(BuildContext context) {
     return Expanded(
-      child: ListView.separated(separatorBuilder:(context, index) => Divider(),
+      child: ListView.separated(separatorBuilder:(context, index) => const Divider(),
       itemCount: otherPages.length,
       itemBuilder: (BuildContext context, int index) {
         return ListTile(
-          title: Text(otherPages[index][0]),
+          title: Text(otherPages[index][0]), // Name of the page
           onTap: () {
-            Navigator.push(
+            Navigator.push( // Navigate to the page once it's clicked
               context,
               MaterialPageRoute(builder: (context) => Consumer<TestModel> (
                 builder: (context, model, child) {
