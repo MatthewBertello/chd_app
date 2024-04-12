98e71e708aeec92a087850ee8dfef5dd296125d2
MatthewBertello <46825957+MatthewBertello@users.noreply.github.com>
2024-04-11 21:24:04 -0500

    Setup Supabase secrets

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ import 'package:chd_app/theme/theme_constants.dart';
 import 'package:chd_app/theme/theme_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(
     MultiProvider(
       providers: [
