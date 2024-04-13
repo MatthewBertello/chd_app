98ebe7843269cc7e84d466e4b648188fef431f61
MatthewBertello <46825957+MatthewBertello@users.noreply.github.com>
2024-04-12 21:31:52 -0500

    start setting up theme colors

--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -1,11 +1,55 @@
 import 'package:flutter/material.dart';
 
-const COLOR_PRIMARY = Color.fromARGB(255, 0, 4, 83);
+// Light color scheme
+ColorScheme lightColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 197, 0, 0),
+);
 
+// Dark color scheme
+ColorScheme darkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 84, 19, 90),
+  );
+
+// Light theme
 ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light
+  brightness: Brightness.light,
+  colorScheme: lightColorScheme,
+  appBarTheme: appBarTheme(colorScheme: lightColorScheme),
+  buttonTheme: buttonTheme(colorScheme: lightColorScheme),
 );
-  
+
+// Dark theme
 ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark
+  brightness: Brightness.dark,
+  colorScheme: darkColorScheme,
+  appBarTheme: AppBarTheme(
+    backgroundColor: darkColorScheme.primary,
+    titleTextStyle: TextStyle(
+        color: darkColorScheme.onPrimary,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold),
+  ),
+
+  buttonTheme: buttonTheme(colorScheme: darkColorScheme),
 );
+
+// AppBar theme
+AppBarTheme appBarTheme({required ColorScheme colorScheme}) {
+  return AppBarTheme(
+    backgroundColor: colorScheme.primary,
+    titleTextStyle: TextStyle(
+        color: colorScheme.onPrimary,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold),
+  );
+}
+
+// Button theme
+ButtonThemeData buttonTheme({required ColorScheme colorScheme}) {
+  return ButtonThemeData(
+    buttonColor: colorScheme.secondary,
+    textTheme: ButtonTextTheme.primary,
+    hoverColor: colorScheme.tertiary,
+  );
+}
