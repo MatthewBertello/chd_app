d5e39f55c454de664039f85e7d64df52a3a645b7
MatthewBertello <46825957+MatthewBertello@users.noreply.github.com>
2024-04-13 02:29:55 -0500

    setup basic login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,12 +2,42 @@ import 'dart:async';
 
 import 'package:chd_app/components/tab_view.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 import 'package:chd_app/main.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  // Create a subscription to listen for a password reset email
+  late final StreamSubscription<void> _authStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _authStateSubscription = supabase.auth.onAuthStateChange.listen((data) {
+      final session = data.session;
+      if (session != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TabView(),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _authStateSubscription.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,20 +55,19 @@ class Login extends StatefulWidget {
                       'https://www.conqueringchd.org/wp-content/uploads/2020/07/awareness-ribbon-300x300.png',
                       width: 100.0,
                       fit: BoxFit.cover)),
-              const TextField(),
-              const TextField(),
-
-              ElevatedButton(
-                // Button that logs user in and navigates to the home screen
-                child: Text('Sign in'),
-                onPressed: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => TabView()));
-                },
+              SupaEmailAuth(
+                onSignInComplete: (response) {},
+                onSignUpComplete: (response) {},
               ),
-
-              // Button for user to change their password incase they forgot their password
-              const TextButton(onPressed: null, child: Text('Forgot Password')),
+              ElevatedButton(
+                onPressed: () => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TabView(),
+                  ),
+                ),
+                child: const Text('Skip Login'),
+              )
             ],
           ),
         ),
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,8 @@
 ///GRACE DO
 import 'package:chd_app/components/default_app_bar.dart';
+import 'package:chd_app/main.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
@@ -8,86 +10,20 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DefaultAppBar(title: 'Settings'),
-      body: SingleChildScrollView(
-        // Wrapped with SingleChildScrollView to avoid overflow when keyboard appears
-        padding: const EdgeInsets.all(75),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment
-              .start, // Adjusted to start to avoid centering vertically
-          crossAxisAlignment:
-              CrossAxisAlignment.start, // Stretch to width of the screen
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Current Email',
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(
-                    horizontal: 10.0, vertical: 15.0), // Add padding
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'New Email',
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(
-                    horizontal: 10.0, vertical: 15.0), // Add padding
-              ),
-            ),
-            const SizedBox(height: 60.0),
-            const SizedBox(height: 16.0),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Current Password',
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(
-                    horizontal: 10.0, vertical: 15.0), // Add padding
-              ),
-              obscureText: true, // Hide password
-            ),
-            const SizedBox(height: 16.0),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'New Password',
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(
-                    horizontal: 10.0, vertical: 15.0), // Add padding
-              ),
-              obscureText: true, // Hide password
-            ),
-            const SizedBox(height: 16.0),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Confirm Password',
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(
-                    horizontal: 10.0, vertical: 15.0), // Add padding
-              ),
-              obscureText: true, // Hide password
-            ),
-            const SizedBox(height: 24.0),
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                foregroundColor: const Color.fromARGB(255, 3, 46, 81),
-              ),
-              child: const Text('Delete Account'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+        appBar: DefaultAppBar(title: 'Settings'),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
               children: [
-                ElevatedButton(
-                onPressed: () {},
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 15.0), // Add padding
-                  child: Text('Submit'),
-                ),
-              ),]
+                SupaResetPassword(
+                  accessToken: supabase.auth.currentSession?.accessToken,
+                  onSuccess: (UserResponse response) {},
+                  onError: (error) {},
+                )
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
