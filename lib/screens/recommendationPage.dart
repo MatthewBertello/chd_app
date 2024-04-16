226ee2dbed27853a704faebcdffbfd9a749a738d
leepachia <pachia0lee@gmail.com>
2024-04-16 01:23:07 -0500

    The tile now displays the recommendation, goal, and educational info.

--- lib/screens/recommendationPage.dart
+++ lib/screens/recommendationPage.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:chd_app/components/default_app_bar.dart';
+import 'package:chd_app/components/tile.dart';
 
 class RecommendationPage extends StatelessWidget {
   final String? recommendation;
   final String? goal;
   final String? educationalInfo;
+  final double innerMargin = 15;
   
-  RecommendationPage({this.recommendation, this.goal, this.educationalInfo});
+  const RecommendationPage({super.key, this.recommendation, this.goal, this.educationalInfo});
 
+  Text makeRecommendationText(BuildContext context) {
+    return Text(
+      '$recommendation\n\n$goal\n\n$educationalInfo'
+    );
+  }
   @override 
   Widget build(BuildContext context) {
-    return Scaffold(appBar: DefaultAppBar(context: context, title: Text(recommendation!)),);
+    return Scaffold(
+      appBar: DefaultAppBar(context: context, title: const Text('Recommendation')),
+      body: Column(
+        children: [
+          Tile(
+            margin: EdgeInsets.all(innerMargin),
+            padding: const EdgeInsets.all(10),
+            child: makeRecommendationText(context),)
+        ],)
+    );
   }
 }
