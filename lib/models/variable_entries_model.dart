e301df04a39ab341fac8a8e221f29f2c595545ff
MatthewBertello <46825957+MatthewBertello@users.noreply.github.com>
2024-04-19 14:07:08 -0500

    update date for variable entries

--- lib/models/variable_entries_model.dart
+++ lib/models/variable_entries_model.dart
@@ -26,10 +26,12 @@ class VariableEntriesModel extends ChangeNotifier {
     await getVariableEntries();
 
     for (var entry in variableEntries) {
-      var variable = variableDefinitions.firstWhere((element) => element['id'] == entry['variable_id']);
+      var variable = variableDefinitions
+          .firstWhere((element) => element['id'] == entry['variable_id']);
       entry['name'] = variable['name'];
       entry['unit'] = variable['unit'];
       entry['description'] = variable['description'];
+      entry['date'] = DateTime.parse(entry['date']);
     }
 
     loaded = true;
@@ -49,17 +51,21 @@ class VariableEntriesModel extends ChangeNotifier {
 
   Future<dynamic> getVariableEntries() async {
     try {
-      variableEntries =
-          await supabase.from('variable_entries').select().eq('user_id', supabase.auth.currentUser!.id);
+      variableEntries = await supabase
+          .from('variable_entries')
+          .select()
+          .eq('user_id', supabase.auth.currentUser!.id);
     } catch (e) {
       print(e);
     }
   }
 
-  List<Map<String, dynamic>> getVariableEntriesFromDate({required DateTime date, required List<Map<String, dynamic>> entries}) {
+  List<Map<String, dynamic>> getVariableEntriesFromDate(
+      {required DateTime date, required List<Map<String, dynamic>> entries}) {
     var filteredEntries = entries.where((element) {
-      DateTime entryDate = DateTime.parse(element['date']);
-      return entryDate.year == date.year && entryDate.month == date.month && entryDate.day == date.day;
+      return element['date'].year == date.year &&
+          element['date'].month == date.month &&
+          element['date'].day == date.day;
     }).toList();
 
     return filteredEntries;
