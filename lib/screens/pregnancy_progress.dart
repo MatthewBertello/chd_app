9e5c80cf28cf6b86fc798e7993aa6703d50d2770
MatthewBertello <46825957+MatthewBertello@users.noreply.github.com>
2024-04-30 13:15:32 -0500

    fix sizing error in the todo list entry

--- lib/screens/pregnancy_progress.dart
+++ lib/screens/pregnancy_progress.dart
@@ -17,66 +17,81 @@ class _PregnancyProgressState extends State<PregnancyProgress> {
   TextEditingController textController = TextEditingController();
 
   void addToToDo(String todo, bool isChecked) {
-    Provider.of<PregnancyModel>(context, listen: false).addToToDo(todo, isChecked);
+    Provider.of<PregnancyModel>(context, listen: false)
+        .addToToDo(todo, isChecked);
     textController.clear();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DefaultAppBar(context: context, title: const Text('Pregnancy Planner')),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TableCalendar(
-            calendarFormat: _calendarFormat,
-            focusedDay: _focusedDay, 
-            firstDay: DateTime(_focusedDay.year, _focusedDay.month, 1 ), // first day of the month
-            lastDay: DateTime(_focusedDay.year, _focusedDay.month + 1, 0), // last day of the month
-            onPageChanged:(focusedDay) {
-              setState(() => _focusedDay = focusedDay);
-            },
-            onFormatChanged:(format) => setState(() => _calendarFormat = format),
-            pageJumpingEnabled: true,
-          ),
-
-           Padding(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Checkbox(value: false, onChanged: null,),
-                SizedBox(
-                  height: 45,
-                  width: 300,
-                  child: TextField(
-                    controller: textController,
-                    decoration: const InputDecoration(
-                    hintText: 'Add to your list',
-                    labelStyle: TextStyle(fontSize: 10)
-                    
-                  ),)),
-                IconButton(icon: const Icon(Icons.add), onPressed: () => addToToDo(textController.text, false),)
-              ],
+        appBar: DefaultAppBar(
+            context: context, title: const Text('Pregnancy Planner')),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TableCalendar(
+              calendarFormat: _calendarFormat,
+              focusedDay: _focusedDay,
+              firstDay: DateTime(_focusedDay.year, _focusedDay.month,
+                  1), // first day of the month
+              lastDay: DateTime(_focusedDay.year, _focusedDay.month + 1,
+                  0), // last day of the month
+              onPageChanged: (focusedDay) {
+                setState(() => _focusedDay = focusedDay);
+              },
+              onFormatChanged: (format) =>
+                  setState(() => _calendarFormat = format),
+              pageJumpingEnabled: true,
             ),
-          ),
-          
-          Expanded(
-            child: ListView.builder(
-              itemCount: Provider.of<PregnancyModel>(context).toDos.length,
-              itemBuilder: (context, index) {
-                
-                return ListTile(
-                  leading: Checkbox(value: Provider.of<PregnancyModel>(context).toDos[index]['isChecked'],
-                  onChanged: (value) => setState(() => isChecked = value?? false)),
-                  title: Text('${Provider.of<PregnancyModel>(context).toDos[index]['todo']}'),
-                  trailing: IconButton(icon: const Icon(Icons.delete), onPressed: () => Provider.of<PregnancyModel>(context, listen: false).deleteToDo(index),)
-                  );
-              }
+            Padding(
+              padding: const EdgeInsets.only(top: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Checkbox(
+                    value: false,
+                    onChanged: null,
+                  ),
+                  SizedBox(
+                      height: 45,
+                      width: MediaQuery.of(context).size.width - 100,
+                      child: TextField(
+                        controller: textController,
+                        decoration: const InputDecoration(
+                            hintText: 'Add to your list',
+                            labelStyle: TextStyle(fontSize: 10)),
+                      )),
+                  SizedBox(
+                      width: 50,
+                      child: IconButton(
+                        icon: const Icon(Icons.add),
+                        onPressed: () => addToToDo(textController.text, false),
+                      ))
+                ],
+              ),
             ),
-          ),
-        ],  
-      )
-    );
+            Expanded(
+              child: ListView.builder(
+                  itemCount: Provider.of<PregnancyModel>(context).toDos.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                        leading: Checkbox(
+                            value: Provider.of<PregnancyModel>(context)
+                                .toDos[index]['isChecked'],
+                            onChanged: (value) =>
+                                setState(() => isChecked = value ?? false)),
+                        title: Text(
+                            '${Provider.of<PregnancyModel>(context).toDos[index]['todo']}'),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () => Provider.of<PregnancyModel>(context,
+                                  listen: false)
+                              .deleteToDo(index),
+                        ));
+                  }),
+            ),
+          ],
+        ));
   }
 }
