95ce87c2025f78e7dcaace3249879ac5dd17e631
leepachia <pachia0lee@gmail.com>
2024-05-03 14:06:56 -0500

    Deleted member and added more to pregnancy model

--- lib/models/main_model.dart
+++ lib/models/main_model.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'member.dart';
 import 'package:chd_app/main.dart';
 
 class MainModel extends ChangeNotifier {
   String test = "hello";
   List membersSearched = [];
-  Member member = Member(
-      name: "Jane Doe",
-      dueDate: DateTime(2024, 9, 1)); // Just a hardcoded member for now
 
   List<Map<String, dynamic>>? variableDefinitions;
 
@@ -15,35 +11,6 @@ class MainModel extends ChangeNotifier {
     final data = await supabase.from('variable_definitions').select();
     notifyListeners();
     return data;
-  }
-
-  // Method to get the countdown for the due date in days
-  int dueDateCountDown() {
-    return member.dueDate!.difference(DateTime.now()).inDays;
-  }
-
-  // Method that counts the number of pregnant days
-  int countDay() {
-    return (dueDateCountDown() - countTotalPregnantDays()).abs();
-  }
-
-  // Method that counts the total number of days in the 9 months of pregnancy
-  int countTotalPregnantDays() {
-    int month =
-        member.dueDate!.month - 9; // Subtract the due dates month from 9 months
-    int year = member.dueDate!.year;
-
-    while (month <= 0) {
-      // If the month is negative or 0 keep adding 12 until it isn't
-      month += 12;
-      year--;
-    }
-
-    // Put together the last time the pregnant person had their period
-    DateTime lastMenstrualPeriod = DateTime(year, month, member.dueDate!.day);
-    return member.dueDate!
-        .difference(lastMenstrualPeriod)
-        .inDays; // Subtract the due date from the last menstrual period
   }
 
   // A method that searches a member depending on the keyword,
