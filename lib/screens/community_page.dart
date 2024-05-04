818286b0cbfb99df526a2f4863835d8c3393e44f
Matthew Steffens <imatts004@gmail.com>
2024-05-04 15:28:59 -0500

    fixed my previous refactor

--- lib/screens/community_page.dart
+++ lib/screens/community_page.dart
@@ -1,6 +1,6 @@
 import 'package:chd_app/screens/question_forum_screen/question_listview.dart';
 import 'package:flutter/material.dart';
-import 'package:chd_app/models/question_forum_model.dart';
+import 'package:chd_app/models/question_forum_model/question_forum_model.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
