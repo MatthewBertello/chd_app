307c5df3c0c839a60c9c2a0d5f6466434a873405
MatthewBertello <46825957+MatthewBertello@users.noreply.github.com>
2024-05-06 17:38:49 -0500

    working radio buttons

--- lib/models/personal_info_model.dart
+++ lib/models/personal_info_model.dart
@@ -8,30 +8,20 @@ class PersonalInfoModel extends ChangeNotifier {
   bool loading = false;
   DateTime selectedDate = DateTime.now();
 
-  Map<String, dynamic> userData = {};
-  List<Map<String, dynamic>> variableItems = [];
-  Map<String, dynamic> userDemographics = {};
-  Map<String, dynamic> userSocials = {};
-  Map<String, dynamic> userPhysicals = {};
-  List<Map<String, dynamic>> demographicsFormFields = [];
-  List<Map<String, dynamic>> socialsFormFields = [];
-  List<Map<String, dynamic>> physicalsFormFields = [];
-
   Map<String, dynamic> userInfo = {};
+  Map<String, Map<String, dynamic>> userInfoMap = {};
   List<Map<String, dynamic>> personalInfoVariables = [];
   Map<String, Map<String, dynamic>> variables = {};
-  List<String> notEnumTypes = ['text', 'date', 'int'];
+  final List<String> notEnumTypes = ['text', 'date', 'int'];
 
   Future<dynamic> reset() async {
     while (loading) {
       continue;
     }
     // Make sure all variables are set back to default
-    userData = {};
-    variableItems = [];
-    userDemographics = {};
-    userSocials = {};
-    userPhysicals = {};
+    userInfo = {};
+    personalInfoVariables = [];
+    variables = {};
     loaded = false;
   }
 
@@ -41,14 +31,10 @@ class PersonalInfoModel extends ChangeNotifier {
     }
     loading = true;
 
-    // await getUserData();
-    // await getUserDemographics();
-    // await getUserSocials();
-    // await getUserPhysicals();
-    // setRadioVariables();
-    // setFormFields(demographicsFormFields, userDemographics);
-    // setFormFields(socialsFormFields, userSocials);
-    // setFormFields(physicalsFormFields, userPhysicals);
+    await getUserInfo();
+    await getPersonalInfoVariables();
+    await prepareUserInfoVariables();
+    addForms();
 
     loaded = true;
     loading = false;
@@ -62,8 +48,10 @@ class PersonalInfoModel extends ChangeNotifier {
   Future<dynamic> getPersonalInfoVariables() async {
     personalInfoVariables = await supabaseModel.getPersonalInfoVariables();
     for (var row in personalInfoVariables) {
-      var rowNoName = row.remove(row['name']);
-      variables[row['name']] = rowNoName;
+      var name = row['name'];
+      row.remove(row['name']);
+      row['key'] = name;
+      variables[name] = row;
     }
   }
 
@@ -72,6 +60,7 @@ class PersonalInfoModel extends ChangeNotifier {
         .from('public_pg_type')
         .select('oid')
         .eq('typname', enumName);
+    print(enumName);
     var oid = oidQuery[0]['oid'];
 
     final valuesQuery = await supabaseModel.supabase!
@@ -85,137 +74,31 @@ class PersonalInfoModel extends ChangeNotifier {
     return values;
   }
 
-  void prepareUserInfo() {
-    // loop through all the keys in the userInfo map
-    for (var key in userInfo.keys) {
-      if (!variables.containsKey(key)) {
-        // If the key is not in the variables map, remove it from the userInfo map
-        userInfo.remove(key);
-      } else {
-        // Otherwise add the values from the variables map to the userInfo map
-        for (var varKey in variables[key]!.keys) {
-          userInfo[key][varKey] = variables[key]![varKey];
-        }
-        if (!notEnumTypes.contains(userInfo[key]['type'])) {
-          userInfo[key]['values'] =
-              getEnumValues(userInfo[key]['type']); // get the enum values
+  Future<dynamic> prepareUserInfoVariables() async {
+    for (var key in variables.keys) {
+      if (userInfo.containsKey(key)) {
+        variables[key]!['value'] = userInfo[key];
+        variables[key]!['title'] = cleanTitle(key);
+        if (!notEnumTypes.contains(variables[key]!['unit'])) {
+          variables[key]!['values'] =
+              await getEnumValues(variables[key]!['unit']);
         } else {
-          userInfo[key]['values'] = userInfo[key]['type'];
+          variables[key]!['values'] = variables[key]!['unit'];
         }
+      } else {
+        variables.remove(key);
       }
     }
   }
 
   void addForms() {
-    for (var key in userInfo.keys) {
-      if (!notEnumTypes.contains(userInfo[key]['type'])) {
-        // add a radio button form
+    for (var key in variables.keys) {
+      if (!notEnumTypes.contains(variables[key]!['unit'])) {
       } else {
-        // add a text form
+        var textField = createTextFormField();
+        variables[key]!['input'] = textField;
       }
     }
-  }
-
-  /////////////////////////////////////////////////////////////////////////////////
-
-  // Gets all of the user's data regarding demographics, social and physical
-  Future<dynamic> getUserData() async {
-    try {
-      userData = (await supabaseModel.supabase!
-              .from('user_info')
-              .select()
-              .eq('user_id', supabaseModel.supabase!.auth.currentUser!.id))
-          .first;
-      print(userData);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  // Gets the users demographics
-  Future<dynamic> getUserDemographics() async {
-    try {
-      // Get all the variables that are demographics
-      List<Map<String, dynamic>> demographics = await supabaseModel.supabase!
-          .from('personal_info_variables')
-          .select('name')
-          .eq('category', 'Demographic');
-
-      // Check the userData and pull out the demographics
-      for (var demographic in demographics) {
-        var variableName = demographic['name'];
-        userDemographics[variableName] = userData[variableName];
-      }
-      print(userDemographics);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  // Get user social variables
-  Future<dynamic> getUserSocials() async {
-    try {
-      // Get all the variables that are social variables
-      List<Map<String, dynamic>> socials = await supabaseModel.supabase!
-          .from('personal_info_variables')
-          .select('name')
-          .eq('category', 'Social');
-
-      // Check the userData and pull out the social variables
-      for (var social in socials) {
-        var variableName = social['name'];
-        userSocials[variableName] = userData[variableName];
-      }
-      print(userSocials);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  // Get user physical variables
-  Future<dynamic> getUserPhysicals() async {
-    try {
-      // Get all the variables that are physical variables
-      List<Map<String, dynamic>> physicals = await supabaseModel.supabase!
-          .from('personal_info_variables')
-          .select('name')
-          .eq('category', 'Physical');
-
-      // Check the userData and pull out the physical variables
-      for (var physical in physicals) {
-        var variableName = physical['name'];
-        userPhysicals[variableName] = userData[variableName];
-      }
-      print(userPhysicals);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  // Map<String, dynamic> userDemographics = {};
-  // Map<String, dynamic> userSocials = {};
-  // Map<String, dynamic> userPhysicals = {};
-
-  // Set the form fields according to the type(userDemographics, userSocials, userPhysicals) passed in
-  void setFormFields(List formFields, Map type) {
-    // for (var entry in type.entries) {
-    //   String title = cleanTitle(entry.key);
-    //   // If any of the variables in the type map is a radio variable, add it to the formFields list
-    //   if (radioVariables.containsKey(entry.key)) {
-    //     formFields.add({
-    //       "title": cleanTitle(entry.key),
-    //       "input": radioVariables[entry.key],
-    //       "isTextFormField": false
-    //     });
-    //   } else {
-    //     // Otherwise just add a textformfield to it
-    //     formFields.add({
-    //       "title": cleanTitle(entry.key),
-    //       "input": createTextFormField(),
-    //       "isTextFormField": true
-    //     });
-    //   }
-    // }
   }
 
   // Cleans the column name from the database for the title of a variable
@@ -245,39 +128,27 @@ class PersonalInfoModel extends ChangeNotifier {
     );
   }
 
-  void setRadioVariables() {
-    // add keys with values of radio buttons(  [{'type': type, 'input': radio button}]   )
-    // radioVariables['race'] = createRadioButtons(raceSelected, races, 'Race');
-    // radioVariables['gender'] =
-    //     createRadioButtons(genderSelected, genders, 'Gender');
-    // radioVariables['marital_status'] = createRadioButtons(
-    //     maritalStatusSelected, maritalStatuses, 'Marital Status');
-    // radioVariables['education_level'] = createRadioButtons(
-    //     educationLevelSelected, educationLevels, 'Education Level');
-    // radioVariables['income_level'] =
-    //     createRadioButtons(incomeLevelSelected, incomeLevels, 'Income Level');
-    // radioVariables['employment_status'] = createRadioButtons(
-    //     employmentStatusSelected, employmentStatuses, 'Employment Status');
-    // radioVariables['language'] =
-    //     createRadioButtons(languageSelected, languages, 'Language');
-  }
-
   List<Widget> createRadioButtons(
       String selectedValue, List variableTypes, String variableCategory) {
     List<Widget> varWithRadioButtons = [];
 
     // Add a radio button to all of the different types in the variable category
-    // for (var type in variableTypes) {
-    //   var radioButton = Radio(
-    //     value: type,
-    //     groupValue: selectedValue,
-    //     onChanged: (value) => saveSelectedToDb(variableCategory, value),
-    //   );
+    for (var type in variableTypes) {
+      var radioButton = Radio<String>(
+        value: type,
+        groupValue: variables[variableCategory]!['value'],
+        onChanged: (value) {
+          print(value);
+          variables[variableCategory]!['value'] = value;
+          // saveSelectedToDb(variableCategory, value);
+          notifyListeners();
+        },
+      );
 
-    //   var title = Text(type);
+      var title = Text(type);
 
-    //   varWithRadioButtons.add(Row(children: [radioButton, title]));
-    // }
+      varWithRadioButtons.add(Row(children: [radioButton, title]));
+    }
 
     return varWithRadioButtons;
   }
