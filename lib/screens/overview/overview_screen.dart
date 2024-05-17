f09af271b9df017a5d89ea69fd71baba13507260
MatthewBertello <46825957+MatthewBertello@users.noreply.github.com>
2024-05-17 04:10:49 -0500

    Fixed a bug where a null string was being split

--- lib/screens/overview/overview_screen.dart
+++ lib/screens/overview/overview_screen.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'recommendationPage.dart';
 import 'package:heart_safe/models/pregnancy_model.dart';
 import 'package:provider/provider.dart';
+
 ///Author: Matthew Bertello, Pachia Lee, Grace Kiesau
 ///Date: 5/14/24
 ///Description: diaplays health status, pregnancy countdown and some recomendations
@@ -21,22 +22,18 @@ class Overview extends StatelessWidget {
   final double innerMargin = 15;
   final double outerPadding = 20;
 
-///This shows the overview screen
+  ///This shows the overview screen
   @override
   Widget build(BuildContext context) {
-    if (Provider.of<MeterModel>(context, listen: false).loaded ==
-            false &&
-        Provider.of<MeterModel>(context, listen: false).loading ==
-            false) {
+    if (Provider.of<MeterModel>(context, listen: false).loaded == false &&
+        Provider.of<MeterModel>(context, listen: false).loading == false) {
       Provider.of<MeterModel>(context, listen: false).init();
     }
-    if (Provider.of<PregnancyModel>(context, listen: false).loaded ==
-            false &&
-        Provider.of<PregnancyModel>(context, listen: false).loading ==
-            false) {
+    if (Provider.of<PregnancyModel>(context, listen: false).loaded == false &&
+        Provider.of<PregnancyModel>(context, listen: false).loading == false) {
       Provider.of<PregnancyModel>(context, listen: false).init();
     }
-    
+
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
@@ -47,39 +44,49 @@ class Overview extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              GestureDetector(  // For the pregnancy countdown
+              GestureDetector(
+                // For the pregnancy countdown
                 behavior: HitTestBehavior.opaque,
-                onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const PregnancyProgress())),
-                child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                  SizedBox(
-                    width: screenWidth / 2 - outerPadding,
-                    height: screenWidth / 2 - outerPadding,
-                    child: PregnancyCountdown(
-                      currentDays: Provider.of<PregnancyModel>(context).currentPregnantDays,
-                      totalDays: Provider.of<PregnancyModel>(context).totalPregnantDays,
-                      margin: EdgeInsets.all(innerMargin),
-                    ),
-                  ),
-                  GestureDetector( // For the health meter
-                    onTap: () => DefaultTabController.of(context).animateTo(1),
-                    child: SizedBox(
-                      width: screenWidth / 2 - outerPadding,
-                      height: screenWidth / 2 - outerPadding,
-                      child: HealthMeter(
-                        value: Provider.of<MeterModel>(context).getTotalStatusPercentage() * 100,
-                        margin: EdgeInsets.all(innerMargin),
+                onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PregnancyProgress())),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        width: screenWidth / 2 - outerPadding,
+                        height: screenWidth / 2 - outerPadding,
+                        child: PregnancyCountdown(
+                          currentDays: Provider.of<PregnancyModel>(context)
+                              .currentPregnantDays,
+                          totalDays: Provider.of<PregnancyModel>(context)
+                              .totalPregnantDays,
+                          margin: EdgeInsets.all(innerMargin),
+                        ),
                       ),
-                      
-                    ),
-                  )
-                ]),
+                      GestureDetector(
+                        // For the health meter
+                        onTap: () =>
+                            DefaultTabController.of(context).animateTo(1),
+                        child: SizedBox(
+                          width: screenWidth / 2 - outerPadding,
+                          height: screenWidth / 2 - outerPadding,
+                          child: HealthMeter(
+                            value: Provider.of<MeterModel>(context)
+                                    .getTotalStatusPercentage() *
+                                100,
+                            margin: EdgeInsets.all(innerMargin),
+                          ),
+                        ),
+                      )
+                    ]),
               ),
-               // Displays all the recommendations
+              // Displays all the recommendations
               Tile(
-                  margin: EdgeInsets.all(innerMargin),
-                  child: displayRecommendations(context),
-                ),
-              
+                margin: EdgeInsets.all(innerMargin),
+                child: displayRecommendations(context),
+              ),
             ],
           ),
         ),
@@ -87,40 +94,43 @@ class Overview extends StatelessWidget {
     );
   }
 
-///Displays recommendations for users based on average values of the rolling data entered (triggered if they are too low)
+  ///Displays recommendations for users based on average values of the rolling data entered (triggered if they are too low)
   Widget displayRecommendations(BuildContext context) {
     if (Provider.of<MeterModel>(context).outOfRangeVars.isEmpty) {
-        return SizedBox(
+      return SizedBox(
           width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height/2,
+          height: MediaQuery.of(context).size.height / 2,
           child: const Center(child: Text('No recommendations to display.')));
     } else {
-        return Column(
-          children: [
-          const Text("Targeted Suggestions", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-            
-          ListView.separated( 
+      return Column(children: [
+        const Text("Targeted Suggestions",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+        ListView.separated(
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
           separatorBuilder: (context, index) => const Divider(),
           itemCount: Provider.of<MeterModel>(context).outOfRangeVars.length,
           itemBuilder: (context, index) {
             return ListTile(
-              // Display each recommendation and corresponding icon
-              title: Text(Provider.of<MeterModel>(context).outOfRangeVars[index]['name']),
-              subtitle: Text('${Provider.of<MeterModel>(context).outOfRangeVars[index]['description'].split('.')[0]}...'),
-              onTap: () => Navigator.push(context, 
-                      MaterialPageRoute(builder: (context) =>
-                      RecommendationPage(name: Provider.of<MeterModel>(context).outOfRangeVars[index]['name'],
-                      recommendation: Provider.of<MeterModel>(context).outOfRangeVars[index]['description'],
-                  ),
-                ),
-              ) 
-            );
+                // Display each recommendation and corresponding icon
+                title: Text(Provider.of<MeterModel>(context)
+                    .outOfRangeVars[index]['name']),
+                subtitle: Text(
+                    '${(Provider.of<MeterModel>(context).outOfRangeVars[index]['description']) ?? "".split('.')[0]}...'),
+                onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RecommendationPage(
+                          name: Provider.of<MeterModel>(context)
+                              .outOfRangeVars[index]['name'],
+                          recommendation: Provider.of<MeterModel>(context)
+                              .outOfRangeVars[index]['description'],
+                        ),
+                      ),
+                    ));
           },
-                ),
-        ]);
+        ),
+      ]);
     }
   }
 }
-
